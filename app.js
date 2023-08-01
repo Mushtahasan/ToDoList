a56c11efe5379926a70a68d1e5b3a81601af336c
Mushtahasan <131141533+Mushtahasan@users.noreply.github.com>
2023-08-01 14:28:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+
+const date=require(__dirname + "/date.js")
+
+const app=express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+const items=["brish","Breakfast","Lecture"];
+let workItems=[];
+
+app.get("/", function(req,res){
+
+   let day=date.getDate();
+   res.render("list",{listTitle:day, newListItems:items});
+})
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List", newListItems:workItems})
+})
+
+app.post("/",function(req,res){
+ 
+    var item=req.body.newItem;
+
+    if(req.body.list==="work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
+   
+
+})
+
+
+
+app.post("/work",function(req,res){
+    let item=req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/about",function(req,res){
+    res.render("about")
+})
+
+app.listen(3000,function(){
+    console.log("server is running at port 3000");
+})
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+app.post("/work",function(req,res){
+//     let item=req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// })
